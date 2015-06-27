7f173437c1a259927614ba733f83cbaa31dfcb73
Asich <amustafin@crystalspring.kz>
2015-06-27 20:35:36 +0600

    Initial Commit

---
+++ Entquiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Entquiz
+//
+//  Created by Mustafin Askar on 27/06/2015.
+//  Copyright (c) 2015 askar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Entquiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Entquiz
+//
+//  Created by Mustafin Askar on 27/06/2015.
+//  Copyright (c) 2015 askar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
